bdca7d4ebaa8cf424f8093532321a78a80877e89
marklim <marklim@localhost>
2024-01-31 00:50:29 +0900

    첫커밋

---
+++ voice_fishing/src/main/java/kr/co/fishing/VoiceFishingApplication.java
@@ -0,0 +1,13 @@
+package kr.co.fishing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceFishingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceFishingApplication.class, args);
+	}
+
+}
---
+++ voice_fishing/src/test/java/kr/co/fishing/VoiceFishingApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.fishing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceFishingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
